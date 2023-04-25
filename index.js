85995a8139423a52c96ad593afe305c684dcfd62
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-04-25 17:00:00 -0500

    adding init function

--- index.js
+++ index.js
@@ -4,32 +4,39 @@ const { Ellipse } = require('./lib/shapes');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'Please type up to three characters as a title for your logo.',
-      name: 'title',
-    },
 
-    {
-      type: 'input',
-      message: 'Please enter a color for your text.',
-      name: 'text-color',
-    },
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'Please type up to three characters as a title for your logo.',
+        name: 'title',
+      },
 
-    {
-      type: 'list',
-      message: 'Please select a shape for your logo from this list.',
-      name: 'shape',
-      choices: [ 'Ellipse', 'Rectangle', 'Circle' ]
-    },
+      {
+        type: 'input',
+        message: 'Please enter a color for your text.',
+        name: 'text-color',
+      },
 
-    {
-      type: 'input',
-      message: `Please type a color for your logo's shape.`,
-      name: 'shape-color',
-    },
+      {
+        type: 'list',
+        message: 'Please select a shape for your logo from this list.',
+        name: 'shape',
+        choices: ['Ellipse', 'Rectangle', 'Circle']
+      },
 
-    
-  ])
+      {
+        type: 'input',
+        message: `Please type a color for your logo's shape.`,
+        name: 'shape-color',
+      },
+
+
+    ])
+
+
+}
+
+init();
