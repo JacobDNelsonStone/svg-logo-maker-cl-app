92f9453d76f6e58e773911b3c1455706459c1705
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-04-26 16:25:47 -0500

    got svg file creation process working with no errors, starting unit testing?

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { Circle } = require('./lib/shapes');
-const { Rectangle } = require('./lib/shapes');
-const { Ellipse } = require('./lib/shapes');
+// const { Circle } = require('./lib/shapes');
+// const { Rectangle } = require('./lib/shapes');
+// const { Ellipse } = require('./lib/shapes');
 const fs = require('fs');
-const inquirer = require('inquirer');
 const { cli } = require('./lib/cli')
 
 
